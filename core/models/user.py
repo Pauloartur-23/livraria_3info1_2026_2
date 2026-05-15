ec4ae3b93e0be3fc8681e7bd8d2900934251858f
Pauloartur-23 <pauloartur.ifc@gmail.com>
2026-05-15 10:39:41 -0300

    feat: inclusão da foto de perfil no usuário

--- core/models/user.py
+++ core/models/user.py
@@ -2,6 +2,8 @@
 Database models.
 """
 
+import re
+
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -9,6 +11,8 @@ from django.contrib.auth.models import (
 )
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
+from uploader.models import Image
 
 
 class UserManager(BaseUserManager):
@@ -49,6 +53,15 @@ class User(AbstractBaseUser, PermissionsMixin):
         default=False,
         verbose_name=_('Usuário é da equipe'),
         help_text=_('Indica que este usuário pode acessar o Admin.'),
+    )
+
+    foto = models.ForeignKey(
+        Image,
+        related_name='user_foto',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        default=None,
     )
 
     objects = UserManager()
